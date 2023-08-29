52742a615fb65ebe511a1c850faba0af76e41798
Orfeas Panagou <orfeas@adadot.com>
2023-08-29 16:47:45 +0300

    completed stripdown

--- src/utils/env/env.ts
+++ src/utils/env/env.ts
@@ -14,5 +14,6 @@ export const env = z
     SLACK_APP_TOKEN: z.string(),
     SLACK_BOT_TOKEN: z.string(),
     API_URL: z.string().optional(),
+    API_KEY: z.string().optional(),
   })
   .parse(process.env);
